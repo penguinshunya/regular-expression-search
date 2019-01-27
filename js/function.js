d98afb4411696fbd04cb9bcaa83e2188a4e6205d
penguinshunya <penguinshunya@hotmail.com>
2019-01-28 01:21:38 +0900

    Refactoring

--- js/function.js
+++ js/function.js
@@ -71,9 +71,9 @@ const collectTextContent = (elems) => {
 };
 
 const blackOrWhite = (hexcolor) => {
-	var r = parseInt(hexcolor.substr(1, 2), 16);
-	var g = parseInt(hexcolor.substr(3, 2), 16);
-	var b = parseInt(hexcolor.substr(5, 2), 16);
+  const r = parseInt(hexcolor.substr(1, 2), 16);
+  const g = parseInt(hexcolor.substr(3, 2), 16);
+  const b = parseInt(hexcolor.substr(5, 2), 16);
 
-	return ((((r * 299) + (g * 587) + (b * 114)) / 1000) < 128) ? "white" : "black";
+  return ((((r * 299) + (g * 587) + (b * 114)) / 1000) < 128) ? "white" : "black";
 };
