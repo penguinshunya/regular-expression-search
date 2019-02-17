5a46b52a1bb041d24254fe2dcfe7a16d73372b60
penguinshunya <penguinshunya@hotmail.com>
2019-02-17 20:53:34 +0900

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -22,15 +22,17 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        loader: "file-loader",
+        options: {
+          outputPath: "asset/image/"
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
+        loader: "file-loader",
+        options: {
+          outputPath: "asset/font/"
+        }
       }
     ]
   }
