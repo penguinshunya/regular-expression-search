72c925300ae20f8ed49b73a5ea88d39cd9b08b33
penguinshunya <takayashunya@gmail.com>
2024-04-13 01:45:52 +0900

    eslintで8と9のどちらを使えばいいかがわからない

--- src/background/index.ts
+++
@@ -1,4 +0,0 @@
-chrome.storage.onChanged.addListener((changes, _) => {
-  const a = 123;
-  const b = a * 2;
-});
--- src/entrypoints/popup/app.tsx
+++ src/entrypoints/popup/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function App() {
-  return <>test</>;
+  return <>hello, world!!!!!!!!!!!!</>;
 }
