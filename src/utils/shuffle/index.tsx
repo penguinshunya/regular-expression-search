131b744938518141cbd80cc04246f22674ca925f
penguinshunya <takayashunya@gmail.com>
2024-07-27 22:21:58 +0900

    add shuffle

---
+++ src/utils/shuffle/index.tsx
@@ -0,0 +1,8 @@
+export default function shuffle<T>(array: T[]): T[] {
+  const result = array.slice();
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
